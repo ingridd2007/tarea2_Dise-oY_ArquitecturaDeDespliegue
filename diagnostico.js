013c20a085ffb64691ff39822fbf28c243b121ca
IngridTorres <ingridtorres201207@gmail.com>
2026-08-17 19:57:17 -0300

    versiones con process + script

--- diagnostico.js
+++ diagnostico.js
@@ -1,4 +1,9 @@
-console.log("Node.js: v24.19.0");
+console.log("Node.js: v24.15.0");
 console.log("npm: 11.17.0");
 console.log("Git: 2.55.0.windows.4");
 console.log("Plataforma: Windows");
+
+console.log(process.version);
+console.log(process.platform);
+console.log(process.argv);
+console.log(process.env);
